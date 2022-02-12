59df5cc80ff8389b95dfe10da7d6d8fad820d7e7
Abhisheksol-12 <85606898+Abhisheksol-12@users.noreply.github.com>
2022-02-12 14:35:48 +0530

    nothing change

--- SignUp/src/app/app.module.ts
+++ SignUp/src/app/app.module.ts
@@ -36,8 +36,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    
+  
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
